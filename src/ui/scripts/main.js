65dd4c4a4c2c17edf1e22db3f37a23dab9dfd5e0
Marc Beyer <marc.beyer@edu.bib.de>
2021-09-21 11:26:21 +0200

    changed options-feacher-div to table

--- src/ui/scripts/main.js
+++ src/ui/scripts/main.js
@@ -1,62 +1,69 @@
 let blockedContainer = document.querySelector("#blocked");
-let faecherContainer = document.querySelector("#faecher");
+let faecherTable = document.querySelector("#faecher tbody");
 let addBtn = document.querySelector("#add-btn");
 
-addBtn.addEventListener("click", (e) => {
+addBtn.addEventListener("click", (e) => {});
 
-});
+let changeFachEventHandler = (e) => {
+    sendChangeDataMessage(
+        element.name,
+        colorInp.value,
+        bgColorInp.value,
+        !checkBox.checked
+    );
+};
+
+function appendWithTd(tableRow, element){
+    let td = document.createElement("td");
+    td.append(element);
+    tableRow.append(td);
+}
+
+function addFachToDOM(element) {
+    let tableRow = document.createElement("tr");
+    let delBtn = document.createElement("button");
+    let lable = document.createElement("lable");
+    let bgColorInp = document.createElement("input");
+    let colorInp = document.createElement("input");
+    let checkBox = document.createElement("input");
+
+    tableRow.className = "fach";
+
+    delBtn.className = "del-Btn";
+    delBtn.append("X");
+    delBtn.addEventListener("click", (e) => {
+        console.log("pressed", element.name);
+        sendDeleteDataMessage(element.name);
+    });
+
+    lable.append(element.name);
+
+    bgColorInp.value = element.bgColor;
+    bgColorInp.type = "color";
+    bgColorInp.addEventListener("change", changeFachEventHandler);
+
+    colorInp.value = element.color;
+    colorInp.type = "color";
+    colorInp.addEventListener("change", changeFachEventHandler);
+
+    checkBox.type = "checkbox";
+    checkBox.checked = !element.isBlocked;
+    checkBox.addEventListener("change", changeFachEventHandler);
+
+    appendWithTd(tableRow, delBtn);
+    appendWithTd(tableRow, lable);
+    appendWithTd(tableRow, bgColorInp);
+    appendWithTd(tableRow, colorInp);
+    appendWithTd(tableRow, checkBox);
+    faecherTable.append(tableRow);
+}
 
 function handleResponse(data) {
     console.log(data);
 
     for (const key in data) {
         if (Object.hasOwnProperty.call(data, key) && key !== "__values") {
-            const element = data[key];
-            let div = document.createElement("div");
-            let delBtn = document.createElement("button");
-            let lable = document.createElement("lable");
-            let bgColorInp = document.createElement("input");
-            let colorInp = document.createElement("input");
-            let checkBox = document.createElement("input");
-
-            div.className = "fach";
-
-            delBtn.className = "del-Btn";
-            delBtn.append("X");
-            delBtn.addEventListener("click", (e) => {
-                console.log("pressed", element.name);
-                sendDeleteDataMessage(element.name);
-            });
-
-            lable.append(element.name);
-
-            bgColorInp.value = element.bgColor;
-            bgColorInp.type = "color";
-            bgColorInp.addEventListener("change", (e) => {
-                console.log("pressed", element.name);
-                sendChangeDataMessage(element.name, colorInp.value, bgColorInp.value, !checkBox.checked);
-            });
-
-            colorInp.value = element.color;
-            colorInp.type = "color";
-            colorInp.addEventListener("change", (e) => {
-                console.log("pressed", element.name);
-                sendChangeDataMessage(element.name, colorInp.value, bgColorInp.value, !checkBox.checked);
-            });
-
-            checkBox.type = "checkbox";
-            checkBox.checked = !element.isBlocked;
-            checkBox.addEventListener("change", (e) => {
-                console.log("pressed", element.name);
-                sendChangeDataMessage(element.name, colorInp.value, bgColorInp.value, !checkBox.checked);
-            });
-
-            div.append(delBtn);
-            div.append(lable);
-            div.append(bgColorInp);
-            div.append(colorInp);
-            div.append(checkBox);
-            faecherContainer.append(div);
+            addFachToDOM(data[key]);
         }
     }
 }
@@ -68,7 +75,7 @@ function handleError(error) {
 function sendGetDataMessage() {
     const sending = browser.runtime.sendMessage({
         type: "get-fach-data",
-        faecher: []
+        faecher: [],
     });
     sending.then(handleResponse, handleError);
 }
@@ -76,7 +83,7 @@ function sendGetDataMessage() {
 function sendDeleteDataMessage(fach) {
     browser.runtime.sendMessage({
         type: "delete-fach-data",
-        fach
+        fach,
     });
 }
 
@@ -85,10 +92,10 @@ function sendChangeDataMessage(name, color, bgColor, isBlocked) {
         type: "change-fach-data",
         fach: {
             name,
-            color, 
+            color,
             bgColor,
-            isBlocked
-        }
+            isBlocked,
+        },
     });
 }
 
@@ -99,8 +106,7 @@ function handleMessage(request, sender, sendResponse) {
             break;
     }
 }
-  
+
 browser.runtime.onMessage.addListener(handleMessage);
 
 sendGetDataMessage();
-
